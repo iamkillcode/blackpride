73dd6b7964bad5aa8acd2d0804a7a79d0f24342e
Emmanuel Oppong <juniorpara1414@gmail.com>
2022-10-29 21:28:56 +0000

    Add files via upload

---
+++ tramscoapp/src/components/ContactCard.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './ContactCard.css'
+
+const ContactCard = () => {
+  return (
+    <div className='containerMain'>
+        ContactCard
+        
+        
+    </div>
+  )
+}
+
+export default ContactCard
--- tramscoapp/src/components/Tracking.jsx
+++ tramscoapp/src/components/Tracking.jsx
@@ -3,39 +3,82 @@ import wwwicon from '../images/Iconfeatherglobe.png'
 import planeicon from '../images/airplane.png'
 import shipicon from '../images/cargo-ship.png'
 import '../components/Tracking.css'
+import ContactCard from './ContactCard'
+
 
 
 const Tracking = () => {
   return (
     <div>
-        <div className='container-1'>
-            <h2>Easily Track Your Shipment</h2>
-            <div>
-                <img src={wwwicon} alt='wwwicon'>
+
+    <div class="parent">
+        <div class="div1"> 
+            <div className='container-1'>
+                    <h1 className='fcolor'>Easily Track Your Shipment</h1>
+            </div>
+        </div>
+
+            
+            <div class="div2"> 
+                <img src={wwwicon} alt='wwwicon' className='flt1'>
                 </img>
-                <p>5+</p>
+                <div className='flt2'>
+                <p className='htag'>5+</p>
                 <p>countries</p>
+                </div>
             </div>
 
-            <div>
-                <img src={shipicon} alt='shipicon'>
+            <div class="div3"> 
+                <img src={shipicon} alt='shipicon' className='flt1'>
                 </img>
-                <p>10+</p>
+                
+                <div className='flt2'>
+                <p className='htag'>10+</p>
                 <p>vessels</p>
+                </div>
             </div>
 
-            <div>
-                <img src={planeicon} alt='planeicon'>
+            <div class="div4"> 
+                <img src={planeicon} alt='planeicon' className='flt1'>
                 </img>
-                <p>5+</p>
+                <div className='flt2'>
+                <p className='htag'>5+</p>
                 <p>plane</p>
+                </div>
+            </div>
+            
+
+            <div class="div5"> 
+            
             </div>
 
-        </div>
-        
-        <div className='container-2'>
+            <div class="div6">
+                <p>
+                    We take great pride in integrating local knowledge
+                    with automated global services.
+                    Phone us now for any inquiries.
+                </p>
+                
+            </div>
 
-        </div>
+            <div class="div7">
+                <button onClick={alert} className="cntbutton">Contact Us</button>
+            
+            </div>
+
+            <div class="div8"> 
+            
+            </div>
+
+            <div class="div9">
+            <div className='container-2'>
+                    <ContactCard />
+                    
+                </div>
+            
+            </div>
+    </div>
+
     </div>
   )
 }
